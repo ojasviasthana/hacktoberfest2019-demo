b9a8950e78282a1ee158868bc7353ba9316bf909
amit <amit.kadivar1@gmail.com>
2019-10-15 23:50:25 +0530

    changes

--- hacktoberfestdemo/urls.py
+++ hacktoberfestdemo/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("demo.urls",namespace="demo"))
-] 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
